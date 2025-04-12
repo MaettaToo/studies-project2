a6ced483bdaaa71cd4b0295bd1d25f6800b97404
MaettaToo <maetta.green2@gmail.com>
2025-04-11 20:33:35 -0500

    not complete

--- control-flow.js
+++ control-flow.js
@@ -4,3 +4,8 @@
  * 0: (introduction)
  * 
  */
+
+
+
+
+
--- string-manipulation.js
+++ string-manipulation.js
@@ -4,3 +4,18 @@
  * 0: (introduction)
  * 
  */
+
+let name1 = 'John Doe';
+let name2 = "John Doe";
+let name3 = `John Doe`;
+
+let num1 = '123';
+let num2 = "123";
+let num3 = `123`;
+
+let sym1 = '&&';
+let sym2 = "&&";
+let sym3 = `&&`;
+
+
+
